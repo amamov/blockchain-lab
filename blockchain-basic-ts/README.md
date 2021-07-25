9b731a1ccaf362e478bea35e8ceb90f4548bd838
amamov <amamov@kakao.com>
2021-07-25 21:50:47 +0900

    [update] project start

--- blockchain-basic-ts/README.md
+++
@@ -1,1 +0,0 @@
-# mini basic blockchain with typescript
--- blockchain-basic-ts/src/app.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config({ path: `${__dirname}/../.env` });
-
-const hello: string = "hello";
-const world: string = process.env.hello || "world";
-
-console.log(hello, world);
