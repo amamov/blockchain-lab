d23bd83cf58469fd6004fc26a472b797175391d7
amamov <amamov@kakao.com>
2021-07-29 21:40:35 +0900

    [update] mining

--- main.py
+++ main.py
@@ -1,8 +1,84 @@
 import logging
 import sys
+import time
+import utils
+import json
+import hashlib
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-logging.info("test")
 
 
-print("test2")
+class BlockChain:
+
+    __MINNG_DIFFICULTY = 3
+
+    def __init__(self):
+        self.transaction_pool = []
+        self.chain = []
+        self.create_block(0, self.hash_block({}))
+
+    def create_block(self, nonce, previous_hash):
+        block = utils.sorted_dict_by_key(
+            {
+                "timestamp": time.time(),
+                "transactions": self.transaction_pool,
+                "nonce": nonce,
+                "previous_hash": previous_hash,
+            }
+        )
+        self.chain.append(block)
+        self.transaction_pool = []
+        return block
+
+    def hash_block(self, block):
+        sorted_block = json.dumps(block, sort_keys=True)  # dictionary sorting : double check
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
+
+    def add_transaction(
+        self, sender_blockchain_address, recipient_blockchain_address, value: float
+    ):
+        transaction = utils.sorted_dict_by_key(
+            {
+                "sender_blockchain_address": sender_blockchain_address,
+                "recipient_blockchain_address": recipient_blockchain_address,
+                "value": float(value),
+            }
+        )
+        self.transaction_pool.append(transaction)
+        return True
+
+    def valid_proof(self, transactions, previous_hash, nonce, difficulty=__MINNG_DIFFICULTY):
+        guess_block = utils.sorted_dict_by_key(
+            {"transactions": transactions, "nonce": nonce, "previous_hash": previous_hash}
+        )
+        guess_hash = self.hash_block(guess_block)
+        return guess_hash[:difficulty] == "0" * difficulty
+
+    def proof_of_work(self):
+        transactions = self.transaction_pool.copy()
+        previous_hash = self.hash_block(self.chain[-1])
+        nonce = 0
+        while self.valid_proof(transactions, previous_hash, nonce) is False:
+            nonce += 1
+        return nonce
+
+
+if __name__ == "__main__":
+    block_chain = BlockChain()
+
+    block_chain.add_transaction("A", "B", 1.0)
+    previous_block = block_chain.hash_block(block_chain.chain[-1])
+    nonce = block_chain.proof_of_work()
+    block_chain.create_block(nonce, previous_block)
+
+    block_chain.add_transaction("C", "D", 3.2)
+    previous_block = block_chain.hash_block(block_chain.chain[-1])
+    nonce = block_chain.proof_of_work()
+    block_chain.create_block(nonce, previous_block)
+
+    block_chain.add_transaction("X", "Y", 12.3)
+    previous_block = block_chain.hash_block(block_chain.chain[-1])
+    nonce = block_chain.proof_of_work()
+    block_chain.create_block(nonce, previous_block)
+
+    utils.print_block_chain(block_chain.chain)
---
+++ utils.py
@@ -0,0 +1,36 @@
+import collections
+import hashlib
+
+
+def sorted_dict_by_key(unsorted_dict):
+    return collections.OrderedDict(sorted(unsorted_dict.items(), key=lambda d: d[0]))
+
+
+def print_block_chain(chains):
+    for idx, chain in enumerate(chains):
+        print(f"{'='*25} Chain {idx} {'='*25}")
+        for key, value in chain.items():
+            if key == "transactions":
+                print(key)
+                for transaction in value:
+                    print(f"{'-'*40}")
+                    for transaction_key, transaction_value in transaction.items():
+                        print(f"{transaction_key:30}{transaction_value}")
+            else:
+                print(f"{key:15}{value}")
+
+    print(f"{'*'*59}")
+
+
+if __name__ == "__main__":
+    print(hashlib.sha256("test".encode()).hexdigest())
+    print(hashlib.sha256("test".encode()).hexdigest())
+    print(hashlib.sha256("test2".encode()).hexdigest())
+    print(hashlib.sha256("test2".encode()).hexdigest())
+
+    block = {"b": 2, "a": 1}
+    print(hashlib.sha256(str(block).encode()).hexdigest())
+    block = {"a": 1, "b": 2}
+    print(hashlib.sha256(str(block).encode()).hexdigest())
+
+    collections.OrderedDict(sorted(block.items(), key=lambda d: d[0]))
